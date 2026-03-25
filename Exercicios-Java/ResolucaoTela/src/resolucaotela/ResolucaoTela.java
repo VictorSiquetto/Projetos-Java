4b74a6c9214c76ecd2a99ea2a1195b9d40b05182
Victor Siquetto <victor.siquetto@gmail.com>
2026-03-24 22:57:49 -0300

    exercicio finalizado

---
+++ Exercicios-Java/ResolucaoTela/src/resolucaotela/ResolucaoTela.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package resolucaotela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author User
+ */
+public class ResolucaoTela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.print("A resolucao da tela é ");
+        System.out.println(d.width + " x " + d.height);
+    }
+    
+}

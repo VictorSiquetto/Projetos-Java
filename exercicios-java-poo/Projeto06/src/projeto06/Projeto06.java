c301ed1c53e0ba508a2af0e57b4388046b321355
Victor Siquetto <victor.siquetto@gmail.com>
2026-08-24 23:09:23 -0300

    renomeando arquivos

--- exercicios-java-poo/Projeto06/src/projeto06/Projeto06.java
+++ exercicios-java-poo/Projeto06/src/projeto06/Projeto06.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package projeto06;
 
-/**
- *
- * @author User
- */
 public class Projeto06 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Lutador l[] = new Lutador[6];
+             
+        l[0] = new Lutador("Lucas", "Espanha", 29, 1.78f, 68.9f, 11, 2, 1);
+        l[1] = new Lutador("Carlos", "Brasil", 31, 1.68f, 57.8f, 14, 2, 3);
+        l[2] = new Lutador("Joao", "Franca", 27, 1.66f, 80.9f, 12, 5, 1);
+        l[3] = new Lutador("Afonso", "EUA", 34, 1.93f, 81.6f, 8, 1, 0);
+        l[4] = new Lutador("Felipe", "Uruguai", 26, 1.82f, 119.3f, 10, 3, 3);
+        l[5] = new Lutador("Pedro", "Japao", 37, 1.75f, 105.7f, 15, 0, 2);
+        
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[5], l[4]);
+        UEC01.lutar();
+        
     }
     
 }
